d9cb78308208b72c4842a807d1c4f74f73ec780c
Juan Antonio González Cano <jantoniogc@gmail.com>
2020-04-28 12:47:03 +0100

    Finalizamos proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
 
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: backgroud-size .8x ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 function App() {
+
+  const [frase, setFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json()
+    setFrase(frase[0]);
+  }
+
+  useEffect(() => {
+    consultarAPI();
+  }, [])
+
   return (
-    <h1>App</h1>
+    <Contenedor>
+      <Frase
+        frase={frase}
+      ></Frase>
+      <Boton
+        onClick={consultarAPI}>
+        obtener Frase
+      </Boton>
+    </Contenedor>
+
   );
 }
 
